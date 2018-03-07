d867a1a1a346cf31316dba7f0a41d1f77c9d9038
Eliran <eliran.kimi@gmail.com>
2018-03-07 20:24:02 +0000

    Changes

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,4 +1,10 @@
 file 'default www' do
 	path '/var/www/html/index.html'
-	content 'Hello MacBook Pro!'
+	content 'Hello MacBook Pro! v2.0'
+end
+
+webnodes =  search('node', 'role:web')
+
+webnodes.each do |node| 
+	puts node
 end
